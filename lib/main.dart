b2c25a880a37359948f117d52be23a1b4cbc28d2
cafefarm-johnny <npcdja@gmail.com>
2022-04-30 01:09:24 +0900

    feat: 메인 위젯 생성

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,17 @@
-void main() {}
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ProfileApp());
+}
+
+class ProfileApp extends StatelessWidget {
+  const ProfileApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
+  }
+}
